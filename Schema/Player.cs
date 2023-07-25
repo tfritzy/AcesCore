9cf956ca5873dfc61063801cea034e78527ef4a9
Tyler <tfritzy@gmail.com>
2023-07-24 21:03:44 -0700

    IDK

--- Schema/Player.cs
+++ Schema/Player.cs
@@ -1,13 +1,21 @@
 using System.Collections.Generic;
 using AcesCore;
+using Newtonsoft.Json;
 
 namespace Schema
 {
     public class Player
     {
+        [JsonProperty("id")]
         public string Id { get; private set; }
+
+        [JsonProperty("displayName")]
         public string DisplayName { get; set; }
+
+        [JsonProperty("hand")]
         public List<Card> Hand;
+
+        [JsonProperty("score")]
         public int Score;
 
         public Player(string id, string displayName)
